d8ee6f3ab65e0375e004d31eefe92a60043f50a7
Loren Heisey <lheisey@cox.net>
2018-08-23 15:21:34 -0700

    Add gulp task to convert readme to markdown

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
+/*jslint node:true*/    // For node.js turn off jslint used before it was defined warnings
+'use strict';
 var gulp = require('gulp');
+var readme = require('gulp-readme-to-markdown');
+
+/**
+* Convert WordPress readme.txt to github readme.md
+*/
+gulp.task('readme', function () {
+    gulp.src([ 'readme.txt' ])
+        .pipe(readme({
+            details: false
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['readme']);
